f0b6402570f8be91ff12c0f1db26847ab1ffab63
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2024-01-05 13:10:04 +0100

    Add tests for endpoints

--- src/test/java/org/michaelheinecke/quarkus/starting/BookResourceTest.java
+++ src/test/java/org/michaelheinecke/quarkus/starting/BookResourceTest.java
@@ -1,20 +1,34 @@
 package org.michaelheinecke.quarkus.starting;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.Test;
+
 @QuarkusTest
 class BookResourceTest {
-    @Test
-    void testHelloEndpoint() {
-        given()
-          .when().get("/api/book")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
+  @Test
+  public void shouldGetAllBooks() {
+    given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).when().get("/api/books").then()
+        .statusCode(Response.Status.OK.getStatusCode()).body("size()", is(4));
+  }
+
+  @Test
+  public void shouldCountAllBooks() {
+    given().header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN).when().get("/api/books/count").then()
+        .statusCode(Response.Status.OK.getStatusCode()).body(is("4"));
+  }
+
+  @Test
+  public void shouldGetABook() {
+    given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).pathParam("id", 1).when()
+        .get("/api/books/{id}").then().statusCode(Response.Status.OK.getStatusCode())
+        .body("title", is("Understanding Quarkus")).body("author", is("Antonio"))
+        .body("yearOfPublication", is(2020)).body("genre", is("IT"));
+  }
 
 }
